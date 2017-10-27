9eff502f5482547119d2268ff416b2c1d40a26f8
pintugorai <33141566+pintugorai@users.noreply.github.com>
2017-10-27 08:43:22 +0530

    Java Code for Cmd run
    Here is a simple java program to run a simple dos command.

---
+++ CmdRun.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+public class CmdRun {
+	public static void main(String[] arg) {
+		try 
+		{ 
+		Process p=Runtime.getRuntime().exec("cmd /c D:\\pintu\\ExecuterTestTry1111.exe"); 
+		p.waitFor(); 
+		BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
+		String line=reader.readLine(); 
+		while(line!=null) 
+		{ 
+		System.out.println(line); 
+		line=reader.readLine(); 
+		} 
+
+		} 
+		catch(IOException e1) {} 
+		catch(InterruptedException e2) {} 
+
+		System.out.println("Done"); 
+	}
+}
